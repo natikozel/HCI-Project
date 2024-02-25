fc1716006992c87b733c170ff3c83e0a2d05df93
natikozel <netanelkozel@gmail.com>
2024-02-25 14:30:01 +0200

    welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { memo } from 'react';
+import type { FC } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import classes from './App.module.css';
+import resets from './components/_resets.module.css';
+import { GalileoDesign } from './components/GalileoDesign/GalileoDesign';
+
+interface Props {
+  className?: string;
 }
-
-export default App;
+export const App: FC<Props> = memo(function App(props = {}) {
+  return (
+      <div className={`${resets.storybrainResets} ${classes.root}`}>
+        <GalileoDesign />
+      </div>
+  );
+});
